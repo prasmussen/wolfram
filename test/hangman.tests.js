25857ead0cb45ff498aac78e7895028776155da5
sander.struijk <sander.struijk@gmail.com>
2013-12-06 10:49:07 +0100

    hangman coding

--- test/hangman.tests.js
+++ test/hangman.tests.js
@@ -8,6 +8,7 @@ describe('hangman', function () {
     describe('init()', function () {
         it('should load all the words from file into dictionary', function (done) {
             hangman.init(function (ordliste) {
+                //noinspection BadExpressionStatementJS
                 ordliste.should.not.be.empty;
                 done();
             });
@@ -19,8 +20,10 @@ describe('hangman', function () {
             it('should return a random word from the dictionary', function () {
                 var word = hangman.pickARandomWord('subst');
                 word.should.not.be.null;
+                word.should.not.be.undefined;
                 word.word.should.not.be.undefined;
                 word.type.should.not.be.undefined;
+                word.type.should.eql('subst');
             });
         });
         describe('pickRandomWord(\'subst\') two times', function () {
@@ -28,6 +31,18 @@ describe('hangman', function () {
                 var firstWord = hangman.pickARandomWord('subst');
                 var secondWord = hangman.pickARandomWord('subst');
                 firstWord.should.not.be.eql(secondWord);
+            });
+        });
+        describe('createWord(string)', function () {
+            it('should return a word object which has attributes and functions to manipulate and test the word', function () {
+                var word = hangman.privates.createWord('SKAPDØR subst');
+                word.should.not.be.null;
+                word.should.not.be.undefined;
+                word.word.should.not.be.undefined;
+                word.type.should.not.be.undefined;
+                word.word.should.eql('SKAPDØR');
+                word.type.should.eql('subst');
+                word.solved.should.be.false;
             });
         });
     });

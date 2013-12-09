d684430b217a5f67fe32a937c55021f1f7783983
sander.struijk <sander.struijk@gmail.com>
2013-12-09 11:24:07 +0100

    made hangman not have the wordlist in memory and rather pick words directly from file

--- actions/hangman.js
+++ actions/hangman.js
@@ -6,35 +6,28 @@
  * Created by sander.struijk on 05.12.13.
  */
 "use strict";
-var wordGenerator = require('../lib/hangman/hangman.wordengine');
+var wordEngine = require('../lib/hangman/hangman.wordengine');
 
 var currentGame = {};
-var filePaths = ['lib/hangman/ordliste.subst.txt',
-                 'lib/hangman/ordliste.verb.txt',
-                 'lib/hangman/ordliste.adv.txt',
-                 'lib/hangman/ordliste.adj.txt'];
 
 function start(type, ctx) {
-    // Very nice, yes
-    type = ['subst', 'verb', 'adv', 'adj'][type.length - 1];
-
+    var nick = ctx.req.source.nick;
     if (currentGame.active) {
-        ctx.callback('Can not start a new game while already playing, stop or end game to start a new one.');
+        ctx.callback('Can not start a new game while already playing, stupid person ' + nick + '! Stop or end game to start a new one.');
         return;
     }
-
     currentGame = newGame(type);
-    ctx.callback('New game started! guess the word: ' + currentGame.word.displayWord());
+    ctx.callback('New game started by ' + nick + '! guess the word: ' + currentGame.word.displayWord());
 }
 
 function newGame(type, attempts) {
     type = type || 'subst';
-    attempts = attempts || 8;
+    attempts = attempts || 5;
     return {
         active: true,
         attempts: attempts,
         players: {},
-        word: wordGenerator.pickARandomWord(type, filePaths)
+        word: wordEngine.pickARandomWord(type)
     };
 }
 
@@ -43,30 +36,30 @@ function stop(ctx) {
     if (currentGame) {
         currentGame.active = false;
         var nick = ctx.req.source.nick;
-        ctx.callback(nick + ' stopped the game, start a new one?');
+        ctx.callback(nick + ' stopped the game, WHY DID YOU STOP THE GAME??!!?!1|||11 anyways want start a new one?');
     }
 }
 
 function answer(c, ctx) {
     if (!currentGame.active) throw new Error('No game running, what was it that you wanted to answer?');
     var nick = ctx.req.source.nick;
-    var guessCount = currentGame.players[nick] || 0;
-    guessCount++;
+    var faildGuessCount = currentGame.players[nick] || 0;
 
     if (currentGame.word.matchChar(c)) {
         if (currentGame.word.solved()) {
             currentGame.active = false;
-            ctx.callback(nick + ' Won the Game! with ' + guessCount + ' attempts, guessing the word: ' + currentGame.word.displayWord());
+            ctx.callback(nick + ' Won the Game! with ' + faildGuessCount + ' failed attempts, guessing the word: ' + currentGame.word.displayWord());
         } else {
             ctx.callback(currentGame.word.displayWord());
         }
     } else {
-        if (guessCount >= currentGame.attempts) {
+        faildGuessCount++;
+        if (faildGuessCount >= currentGame.attempts) {
             currentGame.active = false;
             ctx.callback('Game Over! ' + nick + ' lost the game!');
         }
-        currentGame.players[nick] = guessCount;
-        ctx.callback(nick + ' failed answers: ' + guessCount);
+        currentGame.players[nick] = faildGuessCount;
+        ctx.callback(nick + ' failed answers: ' + faildGuessCount);
     }
 }
 
--- test/hangman.wordengine.tests.js
+++ test/hangman.wordengine.tests.js
@@ -3,46 +3,53 @@
  */
 "use strict";
 var should = require('should'),
-    wordGenerator = require('../lib/hangman/hangman.wordengine'),
-    from = require('fromjs');
+    wordEngine = require('../lib/hangman/hangman.wordengine'),
+    from = require('fromjs'),
+    assert = require('assert');
 
-describe.only('wordGenerator', function () {
-
-    describe('functions', function () {
-        describe('pickRandomWord(type)', function () {
+describe('wordEngine', function () {
+    describe('public', function () {
+        describe('pickARandomWord(type)', function () {
             it('should return a random word from the wordlist', function () {
                 //Arrange and Act
-                var word = wordGenerator.pickARandomWord('subst');
+                var word = wordEngine.pickARandomWord('test');
 
                 //Assert
-                word.should.not.be.undefined;
-                word.should.not.be.undefined;
-                word.word.should.not.be.undefined;
+                word.should.not.be.undefined
+                word.should.be.an.Object;
+                word.word.should.not.be.empty;
                 word.type.should.not.be.undefined;
-                word.type.should.eql('subst');
+                word.type.should.eql('test');
             });
             it('should not return the same word twice, in fact never!', function () {
                 //Arrange and Act
-                var firstWord = wordGenerator.pickARandomWord('subst');
-                //wordGenerator.privates.printDebug();
-                var secondWord = wordGenerator.pickARandomWord('subst');
-                //wordGenerator.privates.printDebug();
+                var firstWord = wordEngine.pickARandomWord('test');
+                //wordEngine.privates.printDebug();
+                var secondWord = wordEngine.pickARandomWord('test');
+                //wordEngine.privates.printDebug();
 
                 //Assert
                 firstWord.should.not.be.eql(secondWord);
             });
-            it('should pick all the 4 words in the list!', function () {
+            it('should pick 4 words in the list!', function () {
+                var words = [];
                 for (var i = 0; i < 4; i++) {
-                    wordGenerator.pickARandomWord('subst');
+                    words.push(wordEngine.pickARandomWord('test'));
+                }
+                words.length.should.eql(4);
+                for (var i = 0; i < 4; i++) {
+                    var w = words[i];
+                    w.should.not.be.undefined;
                 }
             });
         });
-
-        describe('privates', function () {
+    });
+    describe('private', function () {
+        describe('wordFactory', function () {
             describe('createWord(string)', function () {
                 it('should return a word object which has attributes and functions to manipulate and test the word', function () {
                     //Arrange and Act
-                    var word = wordGenerator.privates.createWord('SKAPDØR subst');
+                    var word = wordEngine.privates.wordFactory.createWord('SKAPDØR subst');
 
                     //Assert
                     word.should.not.be.null;
@@ -59,7 +66,7 @@ describe.only('wordGenerator', function () {
             describe('word.matchChar(c)', function () {
                 it('should return false if char is not matched in word, and displayWord should return a string witch no matches for char', function () {
                     //Arrange
-                    var word = wordGenerator.privates.createWord('SKAPDØR subst');
+                    var word = wordEngine.privates.wordFactory.createWord('SKAPDØR subst');
 
                     //Act
                     var match = word.matchChar('O');
@@ -70,7 +77,7 @@ describe.only('wordGenerator', function () {
                 });
                 it('should return true if char is matched in word, and displayWord should reveal all chars in string matching the char', function () {
                     //Arrange
-                    var word = wordGenerator.privates.createWord('SKAPDØR subst');
+                    var word = wordEngine.privates.wordFactory.createWord('SKAPDØR subst');
 
                     //Act
                     var match = word.matchChar('S');
@@ -81,14 +88,14 @@ describe.only('wordGenerator', function () {
                 });
                 it('should throw an exception when trying to match same char twice', function () {
                     (function () {
-                        var word = wordGenerator.privates.createWord('SKAPDØR subst');
+                        var word = wordEngine.privates.wordFactory.createWord('SKAPDØR subst');
                         var match = word.matchChar('S');
                         match = word.matchChar('S');
                     }).should.throwError('Can not match the same char more then once.');
                 });
                 it('should return true if char is matched in word, and displayWord should reveal all chars in string matching the char', function () {
                     //Arrange
-                    var word = wordGenerator.privates.createWord('SKAPTØS subst');
+                    var word = wordEngine.privates.wordFactory.createWord('SKAPTØS subst');
 
                     //Act
                     var match = word.matchChar('S');
@@ -101,7 +108,7 @@ describe.only('wordGenerator', function () {
             describe('when the whole word has been matched', function () {
                 it('word.solved() should return true', function () {
                     //Arrange
-                    var word = wordGenerator.privates.createWord('SKAP subst');
+                    var word = wordEngine.privates.wordFactory.createWord('SKAP subst');
 
                     //Act
                     var match = word.matchChar('S');
@@ -122,7 +129,7 @@ describe.only('wordGenerator', function () {
                 describe.skip('and reset is called afterwards', function () {
                     it('the word should be reset', function () {
                         //Arrange
-                        var word = wordGenerator.privates.createWord('SKAP subst');
+                        var word = wordEngine.privates.wordFactory.createWord('SKAP subst');
                         word.matchChar('S');
                         word.matchChar('K');
                         word.matchChar('A');
@@ -141,56 +148,40 @@ describe.only('wordGenerator', function () {
                     });
                 });
             });
-            describe('getFilePath(type)', function () {
+        });
+        describe('io', function () {
+            before(function () {
+                wordEngine.privates.io.init();
+            });
+            describe('getFilePathMatchingType(type)', function () {
                 it('should return the path containing the type in its filename.', function () {
-                    var path = wordGenerator.privates.getFilePath('subst');
-                    path.should.eql('lib/hangman/ordliste.subst.txt');
+                    var path = wordEngine.privates.io.getFilePathMatchingType('subst');
+                    path.should.eql('lib/hangman/wordlists/subst.nor.wordlist.txt');
                 });
             });
             describe('getLine(type, line_no, callback)', function () {
                 it('should return the line at line number from a text file.', function () {
-                    wordGenerator.privates.getLine('subst', 10, function (err, line) {
-                        if (err) throw err;
-                        if (!line) throw new Error('returned a blank line.');
+                    var path = wordEngine.privates.io.getFilePathMatchingType('test');
+                    var index = wordEngine.privates.io.getRandomLineNumber({type: 'test'});
+                    var word = wordEngine.privates.io.getLine(path, index);
+                    assert.notEqual(word, 'undefined');
+                });
+                describe('getRandomLineNumber(option)', function () {
+                    it('should return a random number between 0 and 4 provided type', function () {
+                        var index = wordEngine.privates.io.getRandomLineNumber({type: 'test'});
+                        assert.notEqual(index, 'undefined');
+                        assert.notEqual(index, 'NaN');
+                    });
+                    it('should return a random number between 0 and 4 provided path', function () {
+                        var path = wordEngine.privates.io.getFilePathMatchingType('test');
+                        var index = wordEngine.privates.io.getRandomLineNumber({path: path});
+                        index.should.be.within(0, 4);
                     });
                 });
-            });
-            describe('splitOnNewLines(data)', function () {
-                it('should split multiple lines into an array of lines.', function () {
-                    var arrayOfSplitLines = wordGenerator.privates.splitOnNewLines('LINE 1\nLINE 2\nLINE 3');
-                    arrayOfSplitLines.length.should.eql(3);
-                    arrayOfSplitLines[0].should.eql('LINE 1');
-                });
-                it('should split multiple lines into an array of lines.', function () {
-                    var arrayOfSplitLines = wordGenerator.privates.splitOnNewLines('LINE 1\r\nLINE 2\r\nLINE 3');
-                    arrayOfSplitLines.length.should.eql(3);
-                    arrayOfSplitLines[0].should.eql('LINE 1');
-                });
-            });
-            describe('createAnonymousWord(word)', function () {
-                it('should return an anonymous word object', function () {
-                    var aw = wordGenerator.privates.createAnonymousWord('WORD');
-                    aw.should.be.an.instanceOf(Array);
-                    aw.length.should.eql(4);
-                    var c = aw[0];
-                    c.anonymousChar.should.eql('_');
-                    c.realChar.should.eql('W');
-                    c.matching.should.be.false;
-                });
-            });
-            describe('getRandomIndex(count, pickedIndexes)', function () {
-                it('should return a random number between 0 and (count - 1) that does not exist in pickedIndexes.', function () {
-                    var index = wordGenerator.privates.getRandomIndex(5, [0, 1, 2, 3]);
-                    index.should.eql(4);
-                });
-                it('should return a random number between 0 and 4', function () {
-                    var index = wordGenerator.privates.getRandomIndex(5, []);
-                    index.should.be.within(0, 4);
-                });
-            });
-            describe.skip('updateLineCount(type)', function () {
-                wordGenerator.privates.updateLineCount('adv', ['lib/hangman/ordliste.adv.txt'], function (lineCount) {
-                    lineCount.should.be.above(0);
+                describe('getLineCount(path)', function () {
+                    var path = wordEngine.privates.io.getFilePathMatchingType('test');
+                    var count = wordEngine.privates.io.getLineCount(path);
+                    count.should.be.eql(4);
                 });
             });
         });

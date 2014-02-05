6bcf767d146143cbacec69815dd74d10ee660fb7
Sander Struijk <sander.struijk@gmail.com>
2014-02-05 20:35:32 +0100

    Added new AssertionError for convenience when testing mandate.

--- test/mandate.tests.js
+++ test/mandate.tests.js
@@ -4,7 +4,7 @@ var should = require('should'),
 
 var TestHelper = function () {
 
-    function humanize(ms){
+    function humanize(ms) {
         if (ms === 0) return 'zero duration';
         var mom = moment.duration(ms);
         var year = mom.years(), month = mom.months(), date = mom.days(), hour = mom.hours(), minutes = mom.minutes(), seconds = mom.seconds(), milliseconds = mom.milliseconds();
@@ -38,9 +38,19 @@ var TestHelper = function () {
         return dateTimeString;
     }
 
+    AssertionError.prototype = Error.prototype;
+    function AssertionError(expected, actual){
+        var error = new Error();
+        error.name = 'AssertionError';
+        error.message = ('expected ' + expected + ' to equal ' + actual);
+        error.expected = expected;
+        error.actual = actual;
+        return error;
+    }
+
     var getDurationTestHelper = function () {
         function paramTest_getDuration_shouldThrowException(input, now, expectedDuration, expectedDateTime, exceptionMessage) {
-            it('should throw an exception with a message matching \'' + exceptionMessage + '\'\n\t when now is ' + now.format() + ' and input is ' + input, function () {
+            it('should throw an exception with a message matching \'' + exceptionMessage + '\' when now is ' + now.format() + ' and input is ' + input, function () {
                 (function () {
                     var actualDuration = mandate.getDuration(input, now);
                     actualDuration.should.eql(expectedDuration);
@@ -53,7 +63,10 @@ var TestHelper = function () {
             it('should return ' + humanize(expectedDuration) + ' when now is ' + now.format() + ' and input is ' + input + '\n' +
                 'and ' + now.format() + ' + ' + humanize(expectedDuration) + ' should match ' + expectedDateTime.format(), function () {
                 var actualDuration = mandate.getDuration(input, now);
-                actualDuration.should.eql(expectedDuration);
+                //actualDuration.should.eql(expectedDuration);
+                if (actualDuration !== expectedDuration) {
+                    throw new AssertionError(humanize(expectedDuration), humanize(actualDuration));
+                }
                 now.add(actualDuration, 'milliseconds').isSame(expectedDateTime).should.be.true;
             });
         }
@@ -91,7 +104,10 @@ var TestHelper = function () {
         function paramTest_parseDateTimeOddbjorn(input, now, expected) {
             it('should return ' + humanize(expected) + ' when input is ' + input + ' and now is ' + moment(now).format(), function () {
                 var actual = mandate._parseDateTimeOddbjorn(input, now);
-                actual.should.eql(expected);
+                //actual.should.eql(expected);
+                if (actual !== expected) {
+                    throw new AssertionError(humanize(expected), humanize(actual));
+                }
             })
         }
 
@@ -120,7 +136,10 @@ var TestHelper = function () {
         function paramTest_parseDateTime(input, now, expected) {
             it('should return ' + humanize(expected) + ' when now is ' + now.format() + ' and input is ' + input, function () {
                 var actual = mandate._parseDateTime(input, now);
-                actual.should.eql(expected);
+                //actual.should.eql(expected);
+                if (actual !== expected) {
+                    throw new AssertionError(humanize(expected), humanize(actual));
+                }
             });
         }
 
@@ -154,13 +173,9 @@ var TestHelper = function () {
 
 describe('mandate', function () {
     //mandate.enableDebug();
-
     var helper, testHelper = new TestHelper();
-
     describe('getDuration', function () {
-
         helper = testHelper.getDurationTestHelper;
-
         var testCases = [
             new helper.TestCase('24:00', moment(), null, null, 'Hours can only be between 0 and 23.'),
             new helper.TestCase('23:59', moment({ h: 0 }), moment.duration({ h: 23, m: 59 }).asMilliseconds(), moment({ h: 23, m: 59, s: 0 }), null),
@@ -174,9 +189,7 @@ describe('mandate', function () {
         ];
         helper.executeTestCases(testCases);
     });
-
     describe('_parseDuration', function () {
-
         function paramTest_parseDuration(input, expected) {
             it('should return ' + expected + 'ms when input is ' + input, function () {
                 var actual = mandate._parseDuration(input);
@@ -186,31 +199,22 @@ describe('mandate', function () {
 
         paramTest_parseDuration('10h', moment.duration(10, 'hours').asMilliseconds());
         paramTest_parseDuration('-10h', 0);
-
     });
-
     describe('parseDateTimeOddbjorn', function () {
-
         helper = testHelper.parseDateTimeOddbjornTestHelper;
-
         var testCases = [
-            new helper.TestCase('24:00', moment().toDate(), 0),
             new helper.TestCase('23:59', moment({ h: 0 }).toDate(), moment.duration({ h: 23, m: 59 }).asMilliseconds()),
             new helper.TestCase('00:00', moment({ h: 0 }).toDate(), 0),
             new helper.TestCase('18:00', moment({ h: 15 }).toDate(), moment.duration({ h: 3 }).asMilliseconds()),
-            new helper.TestCase('18:00', moment({ h: 15 }).toDate(), 0),
-            new helper.TestCase('18:00', moment({ h: 15 }).toDate(), 0),
             new helper.TestCase('24/12 18:00', moment({ M: 10, d: 24 }).toDate(), moment.duration({ d: 30, h: 18 }).asMilliseconds()),
             new helper.TestCase('24/12/2014 18:00', moment({ y: 2013, M: 11, d: 24 }).toDate(), moment.duration({ y: 1, h: 18 }).asMilliseconds()),
-            new helper.TestCase('24/12/2014 18:00', moment({ y: 2015, M: 11, d: 24 }).toDate(), 0)
+            new helper.TestCase('24/12/2014 18:00', moment({ y: 2015, M: 11, d: 24 }).toDate(), 0),
+            new helper.TestCase('20:00', moment({ h: 20, m: 20 }).toDate(), moment.duration({ h: 23, m: 50 }).asMilliseconds())
         ];
         helper.executeTestCases(testCases);
     });
-
     describe('_parseDateTime', function () {
-
         helper = testHelper.parseDateTimeTestHelper;
-
         var testCases = [
             new helper.TestCase('24/12/2014', moment({ y: 2013, M: 11, d: 24 }), moment.duration({ y: 1 }).asMilliseconds()),
             new helper.TestCase('24/12', moment({ M: 10, d: 24 }), moment.duration({ M: 1 }).asMilliseconds()),
